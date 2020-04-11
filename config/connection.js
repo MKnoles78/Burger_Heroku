558d1808e55c29f66c209921135f6fb50ddaa6fa
MKnoles <mknoles78@gmail.com>
2020-04-10 20:54:59 -0400

    removed previous connection

--- config/connection.js
+++ config/connection.js
@@ -15,13 +15,7 @@ if(process.env.JAWSDB_URL){
       });
 
 }
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "RootPassword",
-  database: "cat_db"
-});
+
 
 // Make connection.
 connection.connect(function(err) {

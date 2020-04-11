39c9b756794b1f4f681b4c22ebfe4a40e7bab83f
MKnoles <mknoles78@gmail.com>
2020-04-10 22:57:15 -0400

    updated buttons

--- controllers/burgersControllers.js
+++ controllers/burgersControllers.js
@@ -2,10 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -22,7 +20,6 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.burger_name, req.body.devour
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
@@ -32,7 +29,6 @@ router.delete("/api/burgers", function(req, res) {
   ], [
     req.body.burger_name
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
@@ -46,7 +42,6 @@ router.put("/api/burgers/:id", function(req, res) {
     devour: req.body.devour
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -54,5 +49,4 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
